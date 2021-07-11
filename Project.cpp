ab24fb1849de114f52ba167162dc3a4821ce6217
Pranav <pranavgoel2012@gmail.com>
2021-07-11 22:32:56 +0530

    invoice file corrected, menu formatting using setw

--- Project.cpp
+++ Project.cpp
@@ -4,6 +4,7 @@
 
 // // #include <windows.h>
 #include <bits/stdc++.h>
+#include "product.h"
 #include <conio.h>
 using namespace std;
 enum IN
@@ -19,64 +20,7 @@ enum IN
 // //                   CLASS USED IN PROJECT
 // //****************************************************************
 
-class product
-{
-    int pno;
-    string name;
-    float price, qty, tax, dis;
-
-public:
-    product()
-    {
-        pno = 0;
-        name = "";
-        price = qty = tax = dis = 0.0;
-    }
-    void create_product()
-    {
-        cout << "\nPlease Enter The Product No. of The Product ";
-        cin >> pno;
-        cout << "\n\nPlease Enter The Name of The Product ";
-        cin >> name;
-        // for (int i = name.length() - 1; i <= 20; i++)
-        //     name += " ";
-        // cout << name << "1" << endl;
-        cout << "\nPlease Enter The Price of The Product ";
-        cin >> price;
-        cout << "\nPlease Enter The Discount (%) ";
-        cin >> dis;
-    }
-
-    void show_product()
-    {
-        cout << "\nThe Product No. of The Product : " << pno;
-        cout << "\nThe Name of The Product : ";
-        cout << name;
-        cout << "\nThe Price of The Product : " << price;
-        cout << "\nDiscount : " << dis;
-    }
-
-    int retpno()
-    {
-        return pno;
-    }
-
-    float retprice()
-    {
-        return price;
-    }
-
-    string retname()
-    {
-        return name;
-    }
-
-    int retdis()
-    {
-        return dis;
-    }
-
-}; //class ends here
+//class ends here
 
 //***************************************************************
 //    	global declaration for stream object, object
