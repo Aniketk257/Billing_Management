ff2df8225e3a206e5b1b0933a14e911c5110cbbe
Pranav <pranavgoel2012@gmail.com>
2021-07-12 11:00:05 +0530

    created a header file for pssword

--- Project.cpp
+++ Project.cpp
@@ -5,77 +5,19 @@
 // // #include <windows.h>
 #include <bits/stdc++.h>
 #include "product.h"
-#include <conio.h>
+#include "psswd.h"
 using namespace std;
-enum IN
-{
-    // 13 is ASCII for carriage
-    // return
-    IN_BACK = 8,
-    IN_RET = 13
-
-};
-
-// //***************************************************************
-// //                   CLASS USED IN PROJECT
-// //****************************************************************
-
-//class ends here
 
 //***************************************************************
 //    	global declaration for stream object, object
 //****************************************************************
-
 fstream fp;
-// fstream fp2;
 product pr;
 
 //***************************************************************
 //    	function to write in file
 //****************************************************************
-// Function that accepts the password
-string takePasswdFromUser(char sp = '*')
-{
-    // Stores the password
-    string passwd = "";
-    char ch_ipt;
-    cout << "Enter password: ";
-    // Until condition is true
-    while (true)
-    {
 
-        ch_ipt = getch();
-
-        // if the ch_ipt
-        if (ch_ipt == IN::IN_RET)
-        {
-            cout << endl;
-            return passwd;
-        }
-        else if (ch_ipt == IN::IN_BACK && passwd.length() != 0)
-        {
-            passwd.pop_back();
-
-            // Cout statement is very
-            // important as it will erase
-            // previously printed character
-            cout << "\b \b";
-
-            continue;
-        }
-
-        // Without using this, program
-        // will crash as \b can't be
-        // print in beginning of line
-        else if (ch_ipt == IN::IN_BACK && passwd.length() == 0)
-        {
-            continue;
-        }
-
-        passwd.push_back(ch_ipt);
-        cout << sp;
-    }
-}
 void write_product()
 {
     fp.open("Shop.dat", ios::out | ios::app);
